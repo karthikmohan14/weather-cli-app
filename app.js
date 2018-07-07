63df3450c7a53f60d7013e012b802c40693e90b1
karthikmohan14 <karthikmkochi@gmail.com>
2018-07-07 20:27:31 +0530

    cleanning up with geocode.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const request = require('request');
 const yargs = require('yargs');
+const geocode = require('./geocode/geocode');
 
 const argv = yargs
     .options({
@@ -13,26 +14,10 @@ const argv = yargs
     .help()
     .alias('help', 'h')
     .argv;
-var encodedAddress = encodeURIComponent(argv.address);
-// var encodedAddress = encodeURIComponent('amrita school of engineering coimbatore');
-request({
-    url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-    // url: 'http://maps.googleapis.com/maps/api/geocode/json?address=amrita%20school%20of%20engineering%20coimbatore',
-    json: true
-}, (error, Response, body) => {
-    // console.log(JSON.stringify(body, undefined, 2));
-    if (error) {
-        console.log('unable to connect to server');
-    } else if (body.status === 'ZERO_RESULTS') {
-        console.log('invalid address');
-    } else if (body.status === 'OK') {
-        console.log(`add : ${body.results[0].formatted_address}`);
-        console.log(`lat : ${body.results[0].geometry.location.lat}`);
-        console.log(`lat : ${body.results[0].geometry.location.lng}`);
-    } else if (body.status === 'OVER_QUERY_LIMIT') {
-        console.log('Query over limit. Try again later.');
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+    if (errorMessage) {
+        console.log(errorMessage);
     } else {
-        console.log('unable to connect');
-
+        console.log(JSON.stringify(results, undefined, 2));
     }
 });
---
+++ geocode/geocode.js
@@ -0,0 +1,39 @@
+const request = require('request');
+
+var geocodeAddress = (address, callback) => {
+    var encodedAddress = encodeURIComponent(address);
+    // var encodedAddress = encodeURIComponent('amrita school of engineering coimbatore');
+    request({
+        url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+        // url: 'http://maps.googleapis.com/maps/api/geocode/json?address=amrita%20school%20of%20engineering%20coimbatore',
+        json: true
+    }, (error, Response, body) => {
+        // console.log(JSON.stringify(body, undefined, 2));
+        if (error) {
+            callback('unable to connect to server');
+            // console.log('unable to connect to server');
+        } else if (body.status === 'ZERO_RESULTS') {
+            callback('invalid address');
+            // console.log('invalid address');
+        } else if (body.status === 'OVER_QUERY_LIMIT') {
+            console.log('Query over limit. Try again later.');
+            // console.log('Query over limit. Try again later.');
+        } else if (body.status === 'OK') {
+            // console.log(`add : ${body.results[0].formatted_address}`);
+            // console.log(`lat : ${body.results[0].geometry.location.lat}`);
+            // console.log(`lat : ${body.results[0].geometry.location.lng}`);
+            callback(undefined, {
+                address: body.results[0].formatted_address,
+                latitude: body.results[0].geometry.location.lat,
+                longitude: body.results[0].geometry.location.lng
+            });
+        } else {
+            console.log('unable to connect');
+
+        }
+    });
+};
+
+module.exports = {
+    geocodeAddress
+};
