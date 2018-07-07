f05a50260cfaa7a1fee6fdcc005a1c7840756b73
karthikmohan14 <karthikmkochi@gmail.com>
2018-07-07 20:09:25 +0530

    cli for getting location details

--- app.js
+++ app.js
@@ -10,13 +10,29 @@ const argv = yargs
             string: true
         }
     })
-
+    .help()
+    .alias('help', 'h')
+    .argv;
+var encodedAddress = encodeURIComponent(argv.address);
+// var encodedAddress = encodeURIComponent('amrita school of engineering coimbatore');
 request({
-    url: 'http://maps.googleapis.com/maps/api/geocode/json?address=amrita%20school%20of%20engineering%20coimbatore',
+    url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+    // url: 'http://maps.googleapis.com/maps/api/geocode/json?address=amrita%20school%20of%20engineering%20coimbatore',
     json: true
 }, (error, Response, body) => {
-    console.log(JSON.stringify(body, undefined, 2));
-    console.log(`add : ${body.results[0].formatted_address}`);
-    console.log(`lat : ${body.results[0].geometry.location.lat}`);
-    console.log(`lat : ${body.results[0].geometry.location.lng}`);
+    // console.log(JSON.stringify(body, undefined, 2));
+    if (error) {
+        console.log('unable to connect to server');
+    } else if (body.status === 'ZERO_RESULTS') {
+        console.log('invalid address');
+    } else if (body.status === 'OK') {
+        console.log(`add : ${body.results[0].formatted_address}`);
+        console.log(`lat : ${body.results[0].geometry.location.lat}`);
+        console.log(`lat : ${body.results[0].geometry.location.lng}`);
+    } else if (body.status === 'OVER_QUERY_LIMIT') {
+        console.log('Query over limit. Try again later.');
+    } else {
+        console.log('unable to connect');
+
+    }
 });
