b2d0b1e1515b5cf8e05aa19ddf4777f2063a0480
karthikmohan14 <karthikmkochi@gmail.com>
2018-07-08 08:14:57 +0530

    basic cli to get weather info

--- app.js
+++ app.js
@@ -2,22 +2,38 @@ const request = require('request');
 const yargs = require('yargs');
 const geocode = require('./geocode/geocode');
 
-const argv = yargs
-    .options({
-        a: {
-            demand: true,
-            alias: 'address',
-            describe: 'address to fetch weather for',
-            string: true
-        }
-    })
-    .help()
-    .alias('help', 'h')
-    .argv;
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-    if (errorMessage) {
-        console.log(errorMessage);
-    } else {
-        console.log(JSON.stringify(results, undefined, 2));
-    }
+const apikey = '8a583d6f0146df3fba9b9ce008214b3b';
+
+// const argv = yargs
+//     .options({
+//         a: {
+//             demand: true,
+//             alias: 'address',
+//             describe: 'address to fetch weather for',
+//             string: true
+//         }
+//     })
+//     .help()
+//     .alias('help', 'h')
+//     .argv;
+
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+//     if (errorMessage) {
+//         console.log(errorMessage);
+//     } else {
+//         console.log(JSON.stringify(results, undefined, 2));
+//     }
+// });
+
+// `https://api.darksky.net/forecast/${apikey}/${results.latitude},${results.longitude}`
+var results = {
+    latitude: 10.901849,
+    longitude: 76.901651
+};
+
+request({
+    url: `https://api.darksky.net/forecast/${apikey}/${results.latitude},${results.longitude}`,
+    json: true
+}, (error, response, body) => {
+    console.log(body.currently.temperature);
 });
